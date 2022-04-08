d6d562d5734d77216bb05bc38b705d6dde950945
bluealastor <chrise79456@gmail.com>
2022-04-08 17:19:45 +0200

    7

--- assets/js/second.js
+++ assets/js/second.js
@@ -9,35 +9,34 @@ var id = 0;
 
 if (urlId.has("id")) {
     id = urlId.get("id");
-} else {
-    window.location = "index.html";
 }
 
 const movieDetail = (title, homepage, genres, overview, imgURL) => {
     const divEldetail = document.querySelector("div");
-    const h1ElDetail = document.querySelector("h2");
+    const h2ElDetail = document.querySelector("h2");
     const genresElDetail = document.querySelector("h4");
     const descElDetail = document.querySelector("p");
     const imgElDetail = document.querySelector("img");
 
     imgElDetail.setAttribute("src", "https://image.tmdb.org/t/p/w500/" + imgURL);
     imgElDetail.setAttribute("alt", "poster");
-    h1ElDetail.textContent = title;
+    h2ElDetail.textContent = title;
     linkElDetail.setAttribute("href", homepage);
     genresElDetail.textContent = "Generi: " + genres.map(item => item.name).join(", ");
     descElDetail.textContent = overview;
 };
 
 async function getData() {
-    const response = await fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&?" + id + "api_key=47308f84a54b5d91b280f332b551f284");
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=47308f84a54b5d91b280f332b551f284`);
     return response.json();
 }
 
 
 getData()
     .then((movieInfo) => {
+        // console.log(movieInfo)
         movieDetail(
-            movieInfo.name,
+            movieInfo.title,
             movieInfo.homepage,
             movieInfo.genres,
             movieInfo.overview,
